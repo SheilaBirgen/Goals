666c4928419b658251a50b1807cd83ae92cd42f3
SheilaBirgen <jeronobergen@gmail.com>
2020-01-14 15:50:56 +0300

    create new files alert.service.ts

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Goal } from '../goal';
+import { GoalService }from '../goal-service/goal.service';
 
 @Component({
   selector: 'app-goal',
@@ -8,14 +9,8 @@ import { Goal } from '../goal';
 })
 export class GoalComponent implements OnInit {
 
-  goals: Goal[] = [
-    new Goal(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son',new Date(2019,9,14)),
-    new Goal(2,'Buy Cookies','I have to buy cookies for the parrot',new Date(2019,6,9)),
-    new Goal(3,'Get new Phone Case','Diana has her birthday coming up soon',new Date(2019,1,12)),
-    new Goal(4,'Get Dog Food','Pupper likes expensive snacks',new Date(2019,11,18)),
-    new Goal(5,'Solve math homework','Damn Math',new Date(2019,2,14)),
-    new Goal(6,'Plot my world domination plan','Cause I am an evil overlord',new Date(2019,3,14)),
-  ];
+  goals: Goal[];
+
   toggleDetails(index){
     this.goals[index].showDescription = !this.goals[index].showDescription;
   
@@ -37,7 +32,9 @@ export class GoalComponent implements OnInit {
     this.goals.push(goal)
   }
 
-  constructor() { }
+  constructor(goalService:GoalService) { 
+  //this.goals = goalService.getGoals()
+  }
   
  
   ngOnInit() {
